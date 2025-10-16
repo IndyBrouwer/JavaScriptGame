6ee0a57c670ffd5c0f8e9741ae81d25a55e04292
Indy Brouwer <indycross95@gmail.com>
2025-10-16 11:11:32 +0200

    Player Movement
    Made the player movement, player now follows mouse at all times. Removed the standard assets as well as we wont be using those.

---
+++ JavaScriptGame/agar.js
@@ -0,0 +1,57 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 1200,
+  height: 700,
+  backgroundColor: '#222',
+  physics: {
+    default: 'arcade',
+    arcade: { debug: false }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+
+// let → Can reassign the reference
+
+// const → Reference can’t be reassigned
+
+
+let player;
+let pointer;
+
+const game = new Phaser.Game(config);
+
+
+function preload(){
+    this.load.image('player', 'assets/WhiteCircle.png');
+}
+
+function create() {
+  //Add player in the middle of screen
+  player = this.add.image(640, 360, 'player');
+
+  //Change size of the image
+  player.setScale(0.1);
+
+  //Save reference to the mouse
+  pointer = this.input.activePointer;
+}
+
+function update() {
+  const speed = 5;
+
+  const differencex = pointer.x - player.x;
+  const differencey = pointer.y - player.y;
+
+  //Calculate square root
+  const distance = Math.sqrt(differencex * differencex + differencey * differencey);
+
+  if (distance > 1) {
+    player.x += differencex * 0.1 * speed * (1 / distance);
+    player.y += differencey * 0.1 * speed * (1 / distance);
+  }
+}
