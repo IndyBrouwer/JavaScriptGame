69bedb65990e2207d0b1301fd44f0d9ee357f77e
Indy Brouwer <indycross95@gmail.com>
2025-10-16 13:03:03 +0200

    Food collectables
    Food now spawns randomly within the scene. When the player touches a food collectable, the collectable gets destroyed and the player grows in size ever so slightly.

--- JavaScriptGame/agar.js
+++ JavaScriptGame/agar.js
@@ -28,14 +28,35 @@ const game = new Phaser.Game(config);
 
 function preload(){
     this.load.image('player', 'assets/WhiteCircle.png');
+    this.load.image('food', 'assets/WhiteCircle.png')
 }
 
 function create() {
   //Add player in the middle of screen
-  player = this.add.image(640, 360, 'player');
+  player = this.physics.add.image(640, 360, 'player');
 
   //Change size of the image
   player.setScale(0.1);
+
+
+  // Voeg groep toe voor collectables
+  this.collectables = this.physics.add.group();
+
+  //Spawn food collectables
+  this.time.addEvent({
+    delay: 2000,
+    callback: () => spawnCollectable(this),
+    loop: true
+  });
+
+  this.physics.add.overlap(player, this.collectables, (player, collectable) => {
+    // Verwijder het collectable
+    collectable.destroy();
+
+    // Vergroot de speler een klein beetje
+    player.setScale(player.scale + 0.005);
+  });
+
 
   //Save reference to the mouse
   pointer = this.input.activePointer;
@@ -54,4 +75,13 @@ function update() {
     player.x += differencex * 0.1 * speed * (1 / distance);
     player.y += differencey * 0.1 * speed * (1 / distance);
   }
+}
+
+function spawnCollectable(scene) {
+  const randomx = Phaser.Math.Between(0, scene.sys.game.config.width);
+  const randomy = Phaser.Math.Between(0, scene.sys.game.config.height);
+
+  const ball = scene.collectables.create(randomx, randomy, 'player');
+  ball.setScale(0.05); // kleiner dan speler
+  ball.setTint(Phaser.Display.Color.RandomRGB(100, 255).color); //Geef food collectables een random kleur
 }
